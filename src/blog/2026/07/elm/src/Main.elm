ceb0da7dc7b79a027885793e4bd2d6313f9b66cc
Kayce Basques <kaycebasques@gmail.com>
2026-07-18 14:41:32 -0700

    Update Elm PoC to generate random number

--- src/blog/2026/07/elm/src/Main.elm
+++ src/blog/2026/07/elm/src/Main.elm
@@ -1,7 +1,45 @@
 module Main exposing (main)
 
-import Html exposing (Html, p, text)
+import Browser
+import Html exposing (Html, div, p, text)
+import Random
 
-main : Html msg
 main =
-    p [] [ text "Hello, Elm!" ]
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+
+-- MODEL
+
+type alias Model =
+    { randomNumber : Int }
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { randomNumber = 1 }
+    , Random.generate NewRandomNumber (Random.int 1 1000)
+    )
+
+-- UPDATE
+
+type Msg
+    = NewRandomNumber Int
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NewRandomNumber newInt ->
+            ( { model | randomNumber = newInt }
+            , Cmd.none
+            )
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ p [] [ text ("Your initial random number is: " ++ String.fromInt model.randomNumber) ]
+        ]
